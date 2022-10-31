24c3376adc06bd225a4e2262d96f3a5bb7f3d30e
hetjagani <hetpatel572@gmail.com>
2022-10-31 13:12:02 -0700

    added images to docker-compose file to push to dockerhub

--- auth/src/main/java/com/rideshare/auth/exception/ControllerExceptionHandler.java
+++ auth/src/main/java/com/rideshare/auth/exception/ControllerExceptionHandler.java
@@ -24,120 +24,120 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> exceptionHandler(Exception ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 HttpStatus.INTERNAL_SERVER_ERROR.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<?> userAlreadyExistsExceptionHandler(UserAlreadyExistsException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 HttpStatus.INTERNAL_SERVER_ERROR.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> badRequestExceptionHandler(BadRequestException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserDoesNotExistException.class)
     public ResponseEntity<?> userDoesNotExistExceptionHandler(UserDoesNotExistException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<?> badCredentialsExceptionHandler(BadCredentialsException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<?> disabledUserExceptionHandler(DisabledException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<?> expiredJwtExceptionExceptionHandler(ExpiredJwtException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UnsupportedJwtException.class)
     public ResponseEntity<?> unsupportedJwtExceptionExceptionHandler(UnsupportedJwtException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(MalformedJwtException.class)
     public ResponseEntity<?> malformedJwtExceptionExceptionHandler(MalformedJwtException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(SignatureException.class)
     public ResponseEntity<?> signatureExceptionExceptionHandler(SignatureException ex, WebRequest r) {
-        String uri = ((ServletWebRequest)r).getRequest().getRequestURI().toString();
+        String uri = ((ServletWebRequest) r).getRequest().getRequestURI().toString();
         ErrorResponse errorResponse = new ErrorResponse(new Date(),
                 HttpStatus.UNAUTHORIZED.value(),
                 HttpStatus.UNAUTHORIZED.name(),
                 ex.getMessage(),
-                uri );
+                uri);
 
         return new ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED);
     }
--- ride/src/main/java/com/rideshare/ride/RideApplication.java
+++ ride/src/main/java/com/rideshare/ride/RideApplication.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
